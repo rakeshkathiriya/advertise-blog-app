e7220fb8aa8f9bc7c64ce77e054e4af65f5f2305
vatsal-pytheta <vatsal@pytheta.com>
2025-11-12 16:22:20 +0530

    Remove tailwind.config.ts and add a theme in index.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <h1 className="m-auto w-screen text-center text-red-400">Hello World</h1>;
+  return <h1 className="bg-main m-auto w-screen text-center text-red-400">Hello World</h1>;
 }
 
 export default App;
