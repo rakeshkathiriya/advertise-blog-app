643cbed9b559d675b83942a5f258a9b46115dc57
Rakesh Kathiriya <rkathiriya241@gmail.com>
2025-11-14 12:43:34 +0530

    minor changes

--- backend_api/src/controllers/auth.controller.ts
+++ backend_api/src/controllers/auth.controller.ts
@@ -37,7 +37,7 @@ export const login = async (req: Request, res: Response, next:NextFunction) => {
     res.status(200).json({
       success: true,
       message: "Login successful",
-      data: {userWithoutPassword},
+      data: userWithoutPassword,
     });
   } catch (error: unknown) {
      next(error);
