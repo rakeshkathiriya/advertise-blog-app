3a8ce987148e87dfc80a74f37ed7f33489917f76
Rakesh Kathiriya <rkathiriya241@gmail.com>
2025-11-28 18:30:02 +0530

    minor changes

--- frontend/src/layout/userLayout/MainLayout.tsx
+++ frontend/src/layout/userLayout/MainLayout.tsx
@@ -5,9 +5,9 @@ import { Header } from './Header';
 
 const MainLayout: React.FC = () => {
   return (
-    <div className="bg-bgPrimary/30 flex min-h-screen w-full flex-col items-center justify-center overflow-hidden">
+    <div className="bg-bgPrimary/30 flex min-h-screen w-full flex-col items-center justify-start overflow-hidden">
       <Header />
-      <section className="flex w-full flex-col items-center justify-center">
+      <section className="flex h-full w-full flex-col items-center justify-center">
         <Outlet />
       </section>
       <Footer />
