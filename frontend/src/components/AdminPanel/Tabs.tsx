53199be5b929715c087ed71fc379b42c15584e4f
Rakesh Kathiriya <rkathiriya241@gmail.com>
2025-12-01 11:35:54 +0530

    tabs added

---
+++ frontend/src/components/AdminPanel/Tabs.tsx
@@ -0,0 +1,45 @@
+const Tabs = ({
+  activeTab,
+  setActiveTab,
+}: {
+  activeTab: string;
+  setActiveTab: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+  const handleTabOpen = (tabCategory: string) => {
+    setActiveTab(tabCategory);
+  };
+
+  return (
+    <>
+      <div className="mb-4 border-b border-gray-200">
+        <div role="tablist" className="flex gap-1">
+          <button
+            role="tab"
+            aria-selected="true"
+            className={`hover:text-blue cursor-pointer border-b-2 px-4 py-2 text-sm font-medium transition-colors ${
+              activeTab === 'subscribe_users' ? 'text-blue border-blue' : 'text-textSecondary border-transparent'
+            }`}
+            onClick={() => handleTabOpen('subscribe_users')}
+          >
+            Subscribe Users
+          </button>
+
+          <button
+            role="tab"
+            aria-selected="true"
+            className={`hover:text-blue cursor-pointer border-b-2 px-4 py-2 text-sm font-medium transition-colors ${
+              activeTab === 'forever_subscribe_users'
+                ? 'border-blue text-blue'
+                : 'text-textSecondary border-transparent'
+            }`}
+            onClick={() => handleTabOpen('forever_subscribe_users')}
+          >
+            ForEver Subscribe Users
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Tabs;
--- frontend/src/pages/AdminPages/UserSection/MyUsers.tsx
+++ frontend/src/pages/AdminPages/UserSection/MyUsers.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import HeaderSection from '../../../components/AdminPanel/HeaderSection';
 import Modal from '../../../components/AdminPanel/Modal';
+import Tabs from '../../../components/AdminPanel/tabs';
 import { userSection } from '../../../utils/staticData/staticData';
 import ForEverSubscriptionUsersTable from './ForEverSubscriptionUsersTable';
 import UserForm from './UserForm';
 import UsersTable, { type UserWithIndex } from './UsersTable';
 
 const MyUsers = () => {
+  const [activeTab, setActiveTab] = useState('subscribe_users');
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [editingUser, setEditingUser] = useState<UserWithIndex | null>(null);
   const [searchUser, setSearchUser] = useState<string>('');
@@ -24,80 +26,90 @@ const MyUsers = () => {
     <div className="h-full w-full space-y-4">
       <HeaderSection title={userSection.title} subTitle={userSection.subTitle} />
 
-      {/* Search and Filter Section */}
-      <div className="flex flex-wrap items-center justify-between gap-4">
-        <div className="flex items-center gap-2">
-          <input
-            type="text"
-            placeholder="Search by userName..."
-            value={searchUser}
-            onChange={(e) => setSearchUser(e.target.value)}
-            className="text-textSecondary border-borderMedium focus:ring-borderMedium flex-1 rounded-lg border px-4 py-2 text-sm font-semibold focus:ring-2 focus:outline-none"
-          />
-          <div className="flex items-center gap-2">
-            <label className="text-textSecondary text-sm font-semibold">Status:</label>
-            <select
-              value={statusFilter}
-              onChange={(e) => {
-                setStatusFilter(e.target.value);
-                setCurrentPage(1);
-              }}
-              className="text-textSecondary border-borderMedium focus:ring-borderMedium rounded-lg border px-1 py-2 text-sm font-semibold focus:ring-2 focus:outline-none"
-            >
-              <option value="all">All</option>
-              <option value="active">Active</option>
-              <option value="inactive">Inactive</option>
-            </select>
-          </div>
-          <button
-            onClick={handleSearch}
-            className="text-14 flex items-center gap-2 rounded-full bg-[#aec2d1] px-6 py-2 font-semibold tracking-wide text-[#3a4b66] transition-all duration-500 ease-in-out hover:scale-105 hover:transform"
-          >
-            Search
-          </button>
-        </div>
-      </div>
-
-      <UsersTable setEditingClient={setEditingUser} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
 
       {/* Search and Filter Section */}
-      <div className="flex flex-wrap items-center justify-between gap-4">
-        <div className="flex items-center gap-2">
-          <input
-            type="text"
-            placeholder="Search by userName..."
-            value={searchUser}
-            onChange={(e) => setSearchUser(e.target.value)}
-            className="flex-1 rounded-lg border border-gray-300 px-4 py-2 text-sm font-semibold text-gray-600 focus:ring-2 focus:ring-[#3a4b66] focus:outline-none"
-          />
-          <div className="flex items-center gap-2">
-            <label className="text-sm font-semibold text-[#3a4b66]">Status:</label>
-            <select
-              value={statusFilter}
-              onChange={(e) => {
-                setStatusFilter(e.target.value);
-                setCurrentPage(1);
-              }}
-              className="rounded-lg border border-gray-300 px-1 py-2 text-sm font-semibold text-gray-600 focus:ring-2 focus:ring-[#3a4b66] focus:outline-none"
-            >
-              <option value="all">All</option>
-              <option value="active">Active</option>
-              <option value="inactive">Inactive</option>
-            </select>
+      {activeTab === 'subscribe_users' && (
+        <>
+          <div className="flex flex-wrap items-center justify-between gap-4">
+            <div className="flex items-center gap-2">
+              <input
+                type="text"
+                placeholder="Search by userName..."
+                value={searchUser}
+                onChange={(e) => setSearchUser(e.target.value)}
+                className="text-textSecondary border-borderMedium focus:ring-borderMedium flex-1 rounded-lg border px-4 py-2 text-sm font-semibold focus:ring-2 focus:outline-none"
+              />
+              <div className="flex items-center gap-2">
+                <label className="text-textSecondary text-sm font-semibold">Status:</label>
+                <select
+                  value={statusFilter}
+                  onChange={(e) => {
+                    setStatusFilter(e.target.value);
+                    setCurrentPage(1);
+                  }}
+                  className="text-textSecondary border-borderMedium focus:ring-borderMedium rounded-lg border px-1 py-2 text-sm font-semibold focus:ring-2 focus:outline-none"
+                >
+                  <option value="all">All</option>
+                  <option value="active">Active</option>
+                  <option value="inactive">Inactive</option>
+                </select>
+              </div>
+              <button
+                onClick={handleSearch}
+                className="text-14 flex items-center gap-2 rounded-full bg-[#aec2d1] px-6 py-2 font-semibold tracking-wide text-[#3a4b66] transition-all duration-500 ease-in-out hover:scale-105 hover:transform"
+              >
+                Search
+              </button>
+            </div>
           </div>
-          <button
-            onClick={handleSearch}
-            className="text-14 flex items-center gap-2 rounded-full bg-[#aec2d1] px-6 py-2 font-semibold tracking-wide text-[#3a4b66] transition-all duration-500 ease-in-out hover:scale-105 hover:transform"
-          >
-            Search
-          </button>
-        </div>
-      </div>
-      <ForEverSubscriptionUsersTable
-        setEditingClient={setEditingUser}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
+
+          <UsersTable setEditingClient={setEditingUser} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+        </>
+      )}
+
+      {/* Search and Filter Section */}
+      {activeTab === 'forever_subscribe_users' && (
+        <>
+          <div className="flex flex-wrap items-center justify-between gap-4">
+            <div className="flex items-center gap-2">
+              <input
+                type="text"
+                placeholder="Search by userName..."
+                value={searchUser}
+                onChange={(e) => setSearchUser(e.target.value)}
+                className="flex-1 rounded-lg border border-gray-300 px-4 py-2 text-sm font-semibold text-gray-600 focus:ring-2 focus:ring-[#3a4b66] focus:outline-none"
+              />
+              <div className="flex items-center gap-2">
+                <label className="text-sm font-semibold text-[#3a4b66]">Status:</label>
+                <select
+                  value={statusFilter}
+                  onChange={(e) => {
+                    setStatusFilter(e.target.value);
+                    setCurrentPage(1);
+                  }}
+                  className="rounded-lg border border-gray-300 px-1 py-2 text-sm font-semibold text-gray-600 focus:ring-2 focus:ring-[#3a4b66] focus:outline-none"
+                >
+                  <option value="all">All</option>
+                  <option value="active">Active</option>
+                  <option value="inactive">Inactive</option>
+                </select>
+              </div>
+              <button
+                onClick={handleSearch}
+                className="text-14 flex items-center gap-2 rounded-full bg-[#aec2d1] px-6 py-2 font-semibold tracking-wide text-[#3a4b66] transition-all duration-500 ease-in-out hover:scale-105 hover:transform"
+              >
+                Search
+              </button>
+            </div>
+          </div>
+          <ForEverSubscriptionUsersTable
+            setEditingClient={setEditingUser}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+          />
+        </>
+      )}
 
       {/* Edit Modal */}
       {editingUser && (
