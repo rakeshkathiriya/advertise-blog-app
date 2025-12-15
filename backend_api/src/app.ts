552d111e8036751b0bd5cdbabd9274a517c95c4f
Rakesh Kathiriya <rkathiriya241@gmail.com>
2025-12-15 11:45:00 +0530

    Minor changes

--- backend_api/src/app.ts
+++ backend_api/src/app.ts
@@ -35,7 +35,7 @@ app.use(cookieParser());
 // Enable CORS
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://advertise-blog-app-leww.vercel.app'],
     credentials: true,
   })
 );
