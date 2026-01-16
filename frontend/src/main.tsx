e43b77f354df7ede77fe23db8f95e1d45fa7bd44
Keyur0206 <keyurdhameshiya@gmail.com>
2026-01-16 16:54:55 +0530

    Changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,7 @@
 import { createRoot } from 'react-dom/client';
-import './index.css';
-
 import { Provider } from 'react-redux';
 import { AppProvider } from './appProvider/AppProvider';
+import './index.css';
 import { store } from './store/store';
 
 createRoot(document.getElementById('root')!).render(
